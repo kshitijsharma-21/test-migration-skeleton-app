22e0fd0081b2a2771409a495a820a9caf4f4d2e3
Kshitij Sharma <kshitij.s@osmosys.asia>
2021-11-18 22:53:23 +0530

    Add new migration to check postSchemaChange method execution

--- lib/Migration/Version000001Date20211117102065.php
+++ lib/Migration/Version000001Date20211117102065.php
@@ -13,7 +13,7 @@ use OCP\Migration\SimpleMigrationStep;
 /**
  * Auto-generated migration step: Please modify to your needs!
  */
-class Version000001Date20211117102055 extends SimpleMigrationStep
+class Version000001Date20211117102065 extends SimpleMigrationStep
 {
 	/** @var IDBConnection */
 	protected $db;
@@ -31,34 +31,14 @@ class Version000001Date20211117102055 extends SimpleMigrationStep
 	 */
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper
 	{
-		/** @var ISchemaWrapper */
-		$schema = $schemaClosure();
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
 
 		if ($schema->hasTable('migration_test_users')) {
 			$table = $schema->getTable('migration_test_users');
-			$table->addColumn('user_status', 'boolean', [
-				'notnull' => true,
-				'default' => true,
-			]);
+			$table->dropColumn('status');
 		}
 
-		return $schema;
-	}
-
-	/**
-	 * @param \Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
-	 * @param array $options
-	 * @since 13.0.0
-	 */
-	public function postSchemaChange(IOutput $output, \Closure $schemaClosure, array $options): void
-	{
-		/** @var ISchemaWrapper */
-		$schema = $schemaClosure();
-		$query = $this->db->getQueryBuilder();
-		if ($schema->hasTable('migration_test_users')) {
-			$query->update('migration_test_users')
-					->set('user_status', 'status');
-			$query->execute();
-		}
+        return $schema;
 	}
 }
